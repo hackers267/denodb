fd30f257ee79c85b1eb97482ac64407cf83de267
silence <silence_zhpf@aliyun.com>
2020-09-01 12:07:36 +0800

    Update deps.ts
    change the dept "deno-postgres" to "https://raw.githubusercontent.com/hackers267/deno-postgres/master/mod.ts"

--- deps.ts
+++ deps.ts
@@ -4,7 +4,7 @@ export { default as SQLQueryBuilder } from "https://deno.land/x/dex/mod.ts";
 
 export { camelCase, snakeCase } from "https://deno.land/x/case/mod.ts";
 
-export { Client as PostgresClient } from "https://deno.land/x/postgres/mod.ts";
+export { Client as PostgresClient } from "https://raw.githubusercontent.com/hackers267/deno-postgres/master/mod.ts";
 
 export {
   Client as MySQLClient,
